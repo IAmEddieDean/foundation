ccd0a57b58ad18b5fcda08ba5a79327f69eadb7a
Chris Oyler <christopher.oyler@gmail.com>
2016-06-21 20:50:08 -0700

    resolves #8952

--- js/foundation.tabs.js
+++ js/foundation.tabs.js
@@ -94,10 +94,10 @@ class Tabs {
     this._addKeyHandler();
     this._addClickHandler();
     this._setHeightMqHandler = null;
-    
+
     if (this.options.matchHeight) {
       this._setHeightMqHandler = this._setHeight.bind(this);
-      
+
       $(window).on('changed.zf.mediaquery', this._setHeightMqHandler);
     }
   }
@@ -132,7 +132,7 @@ class Tabs {
 
     this.$tabTitles.off('keydown.zf.tabs').on('keydown.zf.tabs', function(e){
       if (e.which === 9) return;
-      
+
 
       var $element = $(this),
         $elements = $element.parent('ul').children('li'),
@@ -206,7 +206,7 @@ class Tabs {
      * Fires when the plugin has successfully changed tabs.
      * @event Tabs#change
      */
-    this.$element.trigger('change.zf.tabs', [$target]);
+    this.$element.trigger('change.zf.tabs', [$target, $targetContent]);
   }
 
   /**
